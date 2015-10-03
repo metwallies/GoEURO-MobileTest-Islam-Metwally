62a9b0b6c7e1caa6b67c24cddb328d3de8e9d0ae
islam metwally <metwally@islams-MacBook-Pro.local>
2015-10-03 18:59:04 +0200

    Initial Commit

---
+++ GoEuro-MobileTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoEuro-MobileTest
+//
+//  Created by islam metwally on 10/3/15.
+//  Copyright (c) 2015 Islam Metwally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoEuro-MobileTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoEuro-MobileTest
+//
+//  Created by islam metwally on 10/3/15.
+//  Copyright (c) 2015 Islam Metwally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
